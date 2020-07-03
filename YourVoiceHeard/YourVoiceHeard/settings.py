b850cfa84e891462988e856aaae14192053371fd
dalucche <67608277+dalucche@users.noreply.github.com>
2020-07-02 20:36:46 -0700

    Added GCP VM external IP to allowed hosts
    Added GCP VM external IP to allowed hosts so that the Google Cloud Platform can host website

--- YourVoiceHeard/YourVoiceHeard/settings.py
+++ YourVoiceHeard/YourVoiceHeard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.94.213.201']
 
 
 # Application definition
