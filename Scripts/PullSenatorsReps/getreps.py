b446bd149e75ea2a162461508718dce78f24cc9f
Hunter Brown <hujbrown@ucsc.edu>
2020-07-05 13:18:50 -0700

    Now outputs senators and reps into CSV files

--- Scripts/PullSenatorsReps/getreps.py
+++ Scripts/PullSenatorsReps/getreps.py
@@ -7,7 +7,8 @@ To save the info to a file run it like so:
 python getreps.py > <FILENAME>
 """
 
-# import selenium
+# imports
+import csv
 from selenium import webdriver
 
 # Driver Setup
@@ -26,21 +27,27 @@ sen_rows = sen_table.find_elements_by_tag_name('tr')
 # Goes through each row and checks that the length isn't 0
 # and that the state is California
 # Prints the name of the representative and their link
-print('Senators')
-for row in sen_rows:
-    senator = row.find_elements_by_tag_name('td')
-    if len(senator) < 1:
-        continue
-    if senator[1].text == 'California':
-        print(senator[0].text)
-        print(senator[0].find_element_by_tag_name('a').get_attribute('href'))
-print()
+with open('senators.csv', 'w', newline='') as csvfile:
+    sen_writer = csv.writer(csvfile, delimiter = ',')
+    sen_writer.writerow(['Senator', 'State', 'Website'])
+    print('Senators')
+    for row in sen_rows:
+        senator = row.find_elements_by_tag_name('td')
+        if len(senator) < 1:
+            continue
+        if senator[1].text == 'California':
+            name = senator[0].text
+            website = senator[0].find_element_by_tag_name('a').get_attribute('href')
+            sen_writer.writerow([name, 'California', website])
+            print(name,website)
+    print()
 # Representatives
 driver.get(reps)
 
 # Gets all the representatives tables
 rep_tables = driver.find_elements_by_tag_name('table')
 cali_table = rep_tables[0]
+
 
 print("Representatives")
 # Looks for the california table
@@ -51,12 +58,19 @@ for table in rep_tables:
         break
 # Gets each row of the california representatives table and prints the name and link
 cali_reps = cali_table.find_elements_by_tag_name('tr')
-for rep in cali_reps:
-    rep_data = rep.find_elements_by_tag_name('td')
-    if len(rep_data) < 1:
-        continue
-    print(rep_data[1].text.replace('(link is external)', ''))
-    print(rep_data[1].find_element_by_tag_name('a').get_attribute('href'))
+
+with open('representatives.csv', 'w', newline='') as csvfile:
+    rep_writer = csv.writer(csvfile, delimiter = ',')
+    rep_writer.writerow(['Representative', 'State', 'Website', 'Committee'])
+    for rep in cali_reps:
+        rep_data = rep.find_elements_by_tag_name('td')
+        if len(rep_data) < 1:
+            continue
+        name = rep_data[1].text.replace('(link is external)', '')
+        website = rep_data[1].find_element_by_tag_name('a').get_attribute('href')
+        committee = rep_data[5].text
+        rep_writer.writerow([name, 'California', website, committee])
+        print(name,website,committee)
 
 
 # Closes the driver
