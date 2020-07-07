811646d648415de0f195459ca98849bd3a17e027
rivatadutta <67610418+rivatadutta@users.noreply.github.com>
2020-07-07 13:10:40 -0700

    Update urls.py

--- YourVoiceHeard/emailreps/urls.py
+++ YourVoiceHeard/emailreps/urls.py
@@ -6,4 +6,7 @@ app_name = 'emailreps'
 urlpatterns = [
     path('', views.index, name='index'),
     path('emailofficials', views.contact_reps, name='contact_reps'),
+    path('aboutsenators', views.senator_page, name='SenatorPage'),
+    path('proposeissues', views.propose_vote_issues, name='propose_vote_issues'),
+    path('aboutreps', views.reps_page, name='reps_page'),
 ]
