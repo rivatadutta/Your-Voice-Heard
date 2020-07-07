eded178d70067c2ce2fc71c30ba8fe57be4afc49
Hunter Brown <hujbrown@ucsc.edu>
2020-07-07 14:29:56 -0700

    Fixed links and duplicate files

--- YourVoiceHeard/YourVoiceHeard/emailreps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'emailreps'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('emailofficials', views.contact_reps, name='contact_reps'),
-]
