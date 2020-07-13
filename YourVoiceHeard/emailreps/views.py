a2c588e267b7d4d8810b22f9f36a5eed59fdb20d
Hunter Brown <hujbrown@ucsc.edu>
2020-07-13 15:00:31 -0700

    Fixed Number of Votes appearing as None

--- YourVoiceHeard/emailreps/views.py
+++ YourVoiceHeard/emailreps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from django.db.models import Avg, Count, Min, Sum
+from django.db.models.functions import Coalesce
 from .models import Senator, Representative, Issue, Vote
 from .forms import IssueForm
 
@@ -88,7 +89,7 @@ def downvote(request, id):
 
 def results(request):
     # need to count the number of vote objects for a given issue
-    curr_issues = Issue.objects.annotate(true_vote=Sum('vote__vote_type')).order_by('-true_vote')
+    curr_issues = Issue.objects.annotate(true_vote=Coalesce(Sum('vote__vote_type'), 0)).order_by('-true_vote')
     context = {'issues': curr_issues}
     return render(request, 'results.html', context)
 
